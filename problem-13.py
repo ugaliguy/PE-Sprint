405ddd70286b5acfb6f7d01e2f0c3a2f3e525093
Chris Gomes <ugali_guy@hotmail.com>
2016-08-14 15:46:42 -0500

    Submit code for sum of 50-digit numbers

--- problem-13.py
+++ problem-13.py
@@ -2,4 +2,11 @@
 
 # Large Sum
 
+
+# Learn how to deal with input/output
 large = [map(int, s.split()) for s in open('problem-13-grid.txt').readlines()]
+
+#large = [int(i) for i in large.split("\n")]
+
+big_sum = sum(large)
+print(big_sum) # 5537376230390876637302048746832985971773659831892672
