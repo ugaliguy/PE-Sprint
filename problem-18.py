8b6686c97e111750db3e34e645629cc76a80dd2e
Chris Gomes <ugali_guy@hotmail.com>
2016-09-29 23:42:36 -0500

    Submit code using  input reader and clever bottom-up algorithm

---
+++ problem-18.py
@@ -0,0 +1,10 @@
+# Python 3
+
+pyramid = [map(int, s.split()) for s in open('problem-18-pyramid.txt').readlines()]
+
+# Start at last row - i.e. the bottom - and move to the top
+for row in range(len(pyramid)-1, 0, -1): 
+    for col in range(row):
+        pyramid[row-1][col] += max(pyramid[row][col], pyramid[row][col+1])
+
+print "Maximum = " +  str(table[0][0])
